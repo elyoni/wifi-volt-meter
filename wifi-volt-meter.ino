809f799d53cd55bdd95a048064cfb8ffb20f71d9
Yoni Elentok <elyoni@gmail.com>
2017-08-12 08:51:08 +0300

    update

--- wifi-volt-meter.ino
+++ wifi-volt-meter.ino
@@ -13,21 +13,24 @@ const char *ver = "ver08";
 uint8_t lastConnection;
 //char *readChar;
 char readStringa[] = "1";
+char splitSign = 127;       // Split between samples
 
 char* decimalToArrayChar(int value){
   //The Maximum Size is 4 Byte
   //The Websocket can't get chars that there index is greater then 127
   //When I have a character that the index is equle to 0 the string will be cut
   //To fix that i am using the number 1 to 127 and not 0 to 127
+  //The charater 127 will sign new sample
+
   //then the maximum size of the data can be lower or equle to 5
 
   // Example, The number 30287 will be send by 3 chars:
   //    array[0] = '>', array[1] = 'p' and array[2] = '\x02' 
   static char array[5] = {0,0,0,0,0} ;
   for(int i = 0 ;  value != 0 ; i++){
-    array[i] = (value % 127)+1;
+    array[i] = (value % 126)+1;
     //Serial.print(array[i]);
-    value = (value / 127);
+    value = (value / 126);
   }
   return array;
 }
@@ -47,9 +50,17 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
         Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
         webSocket.sendTXT(num, "Connected");
 
-        String textToSend = String(decimalToArrayChar(30287));
-        Serial.println(textToSend);
-        webSocket.sendTXT(lastConnection, textToSend);
+/*        for(int j = 0 ; j < 10 ; j++){
+          String textToSend = "";
+          for(int i = 0 ; i < 50 ; i++){
+            //textToSend += String(decimalToArrayChar(30000+i));
+            //textToSend += splitSign;
+            textToSend += String(i);
+            textToSend += ",";
+          }
+          webSocket.sendTXT(lastConnection, textToSend);
+          Serial.println(textToSend);
+        }*/
         break;
 
       }
@@ -87,9 +98,10 @@ void setup() {
 
 void loop() {
   webSocket.loop();
-  //char* arr = decimalToArrayChar(10,4);
-  //  char A = 243;
-  //Serial.println(A);
-  //  String textToSend = String(A);
-  // webSocket.sendTXT(lastConnection, textToSend);
+  String textToSend = "";
+  for(int i = 0 ; i < 50 ; i++){
+    textToSend += String(decimalToArrayChar(millis()));
+    textToSend += splitSign;
+  }
+  webSocket.sendTXT(lastConnection, textToSend);
 }
