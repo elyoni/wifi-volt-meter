1f8c5c203f4ab11804e99df46366e5589376595c
Johnny-PC-Linux <elyoni@gmail.com>
2017-08-30 20:18:33 +0300

    I create a class SmartWifi that handle the connection to the network. It use the data that store in the EEPROM

--- WithoutGraph/wifi-volt-meter-var01/wifi-volt-meter-var01.ino
+++ WithoutGraph/wifi-volt-meter-var01/wifi-volt-meter-var01.ino
@@ -11,64 +11,205 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 
 //Don't change the flowing value
 //The data will be loaded from the EEPROM
-char ssid[32];
-char password[32];
+
+//char ssid[32];
+//char password[32];
 
 const char newDeviceSign = 42 ;
 
 const int maxConnectionTries = 40;
 int connectionTries = 0;
-ESP8266WebServer server(80);
+ESP8266WebServer webServer(80);
 
 bool isConnected = false;
 
 //User Can change
-const int PCF8591 = 0x48; //I2C address
-const byte numberOfAnalog = 4; 
-char deviceSign = '1' ;
+const int PCF8591 = 0x48; //#TODO will be pull from the eeprom I2C address
+const byte numberOfAnalog = 4;  //#TODO Will pull from the EEPROM 
+char deviceSign = '1' ;  //#TODO  Change the sign to IP address
 
 
+class SmartWifi{
+  //I am using the EEPROM to save the data off the access point and the password
+  static const int SSID_SIZE = 16;
+  static const int PASSWORD_SIZE = 20;
+  private:
+    int _startAddress; // Save the point where to start save the AP and the Pass
+    int _eepromSize;
+    char _ssid[SSID_SIZE];
+    char _password[PASSWORD_SIZE];
+    int _maxNumberOfTries;
+    bool _useSerial;
+    IPAddress  _ipAddress;
+    ESP8266WebServer& _webServer;
+    void loadCredentiasFromEEPROM(void);
+    void saveCredentialsToEEPROM(void);
+    void clearCredentiasFromEEPROM(void);
+    // Site handler functions 
+    void accessPointAndPasswordForm(void);
+    void resetESPForm(void);
+    void resetESPHandler(void);
+  public:
+    SmartWifi(int startAddress,int eepromSize,bool useSerial,ESP8266WebServer& webServer);
+    SmartWifi(bool useSerial, int eepromSize,ESP8266WebServer& webServer);
+    void setMaxNumberOfConnectionTries(int numberOfTries);
+    void connectToWifi();
 
+};
+SmartWifi::SmartWifi(bool useSerial, int eepromSize, ESP8266WebServer& webServer):_useSerial(useSerial),_eepromSize(eepromSize),_webServer(webServer){
+  _maxNumberOfTries = 20;
+  _startAddress = 0;
+  loadCredentiasFromEEPROM();
+}
+SmartWifi::SmartWifi(int startAddress,int eepromSize, bool useSerial, ESP8266WebServer& webServer):_useSerial(useSerial), _startAddress(startAddress), _eepromSize(eepromSize),_webServer(webServer){
+  _maxNumberOfTries = 20;
+  loadCredentiasFromEEPROM();
+}
 
+void SmartWifi::loadCredentiasFromEEPROM(void){
 /** Load WLAN credentials from EEPROM */
-void loadCredentials() {
-  EEPROM.begin(512);
-  EEPROM.get(0, ssid);
-  EEPROM.get(0+sizeof(ssid), password);
-  char ok[2+1];
-  EEPROM.get(0+sizeof(ssid)+sizeof(password), ok);
+  char ok[3];
+  EEPROM.begin(_eepromSize);
+  EEPROM.get(_startAddress, _ssid);
+  EEPROM.get(_startAddress+sizeof(_ssid), _password);
+  EEPROM.get(_startAddress+sizeof(_ssid)+sizeof(_password), ok);
   EEPROM.end();
   if (String(ok) != String("OK")) {
-    ssid[0] = 0;
-    password[0] = 0;
+    _ssid[0] = 0;
+    _password[0] = 0;
   }
-  Serial.println("Recovered credentials:");
-  Serial.println(ssid);
-  Serial.println(strlen(password)>0?"********":"<no password>");
+  if (_useSerial){
+    Serial.println("Recovered credentials:");
+    Serial.println(_ssid);
+    Serial.println(strlen(_password)>0?"********":"<no password>");
+  }
 }
 
-/** Store WLAN credentials to EEPROM */
-void saveCredentials() {
-  EEPROM.begin(512);
-  EEPROM.put(0, ssid);
-  EEPROM.put(0+sizeof(ssid), password);
-  char ok[2+1] = "OK";
-  EEPROM.put(0+sizeof(ssid)+sizeof(password), ok);
+void SmartWifi::saveCredentialsToEEPROM(void){
+  char ok[3] = "OK";
+  EEPROM.begin(_eepromSize);
+  EEPROM.put(_startAddress, _ssid);
+  EEPROM.put(_startAddress+sizeof(_ssid), _password);
+  EEPROM.put(_startAddress+sizeof(_ssid)+sizeof(_password), ok);
+  EEPROM.commit(); //After using put we need to use commit
+  EEPROM.end();
+}
+
+void SmartWifi::clearCredentiasFromEEPROM(void){
+  String empty = "0000"; //Enter junk data
+  empty.toCharArray(_ssid,SSID_SIZE);
+  empty.toCharArray(_password,PASSWORD_SIZE);
+
+  char no[2+1] = "NO";
+  EEPROM.begin(_eepromSize);
+  EEPROM.put(_startAddress, _ssid);
+  EEPROM.put(_startAddress+sizeof(_ssid), _password);
+  EEPROM.put(_startAddress+sizeof(_ssid)+sizeof(_password), no);
   EEPROM.commit();
   EEPROM.end();
 }
 
-void clearCredentials(){
-  String empty = "123123"; //Enter junk data
-  empty.toCharArray(ssid,32);
-  empty.toCharArray(password,16);
-  EEPROM.begin(512);
-  EEPROM.put(0, ssid);
-  EEPROM.put(0+sizeof(ssid), password);
-  char no[2+1] = "OK";
-  EEPROM.put(0+sizeof(ssid)+sizeof(password), no);
-  EEPROM.commit();
-  EEPROM.end();
+void SmartWifi::setMaxNumberOfConnectionTries(int maxNumberOfTries){
+  _maxNumberOfTries = maxNumberOfTries;
+}
+
+
+void SmartWifi::accessPointAndPasswordForm(void){
+  char site[300];
+  snprintf(site,300,
+      "<html>\
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+      <body>\
+      <h1>Update Access Point Settings</h1>\
+      <form action=\"/resetESPForm\">\
+      Access Point:<input type=\"text\" name=\"Access Point\">\
+      <br>\
+      Password:<input type=\"text\" name=\"Password\">\
+      <br>\
+      <input type=\"submit\" value=\"Update\">\
+      </form>\
+      </body>\
+      </html>"
+      );
+  _webServer.send(200,"text/html",site);
+}
+
+void SmartWifi::connectToWifi(void){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(_ssid,_password);
+  //Tring to connect to the wifi
+  int connectionTries = 0;
+  while(WiFi.status() != WL_CONNECTED && connectionTries < _maxNumberOfTries){
+    delay(500);
+    if (_useSerial){
+      Serial.print(".");
+    }
+    connectionTries++;
+  }
+  if (connectionTries == _maxNumberOfTries){
+    // Could not success to connect to the wifi
+    // Switching To hotspot
+    if (_useSerial){
+      Serial.println("");
+      Serial.println("Couldn't Connect to the Wifi, changing to Access Point option");
+    }
+    WiFi.disconnect();
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("esp8266 Access Point"); //For some reasone the password wan't work
+    _webServer.on("/",[this](){accessPointAndPasswordForm();});
+    _webServer.on("/settingsAPPA",[this](){accessPointAndPasswordForm();});
+    _webServer.on("/resetESPForm",[this](){resetESPForm();});
+    _webServer.on("/resetESP", [this](){resetESPHandler();});
+    _webServer.begin();
+    while(true){
+      //The user need to reset the ESP to after he update the ssid and the password
+      _webServer.handleClient();
+    }
+  }else{
+    // Every things is good, continue with the program
+    if (_useSerial){Serial.println(WiFi.localIP());}
+    _ipAddress = WiFi.localIP();
+   
+   /*
+   // i2c config
+    Wire.begin(0,2);
+    // webSocket config
+    _webSocket.begin();
+    _webSocket.onEvent(webSocketEvent);
+    */
+  }
+}
+
+
+void SmartWifi::resetESPForm(void){
+  Serial.println(_webServer.argName(0) + ": " + _webServer.arg(0));
+  Serial.println(_webServer.argName(1) + ": " + _webServer.arg(1));
+
+  _webServer.arg(0).toCharArray(_ssid,32);
+  _webServer.arg(1).toCharArray(_password,16);
+  /*
+     --- Here Come the code for saving the data into the EEPROM
+   */
+  saveCredentialsToEEPROM();
+  char site[500];
+  snprintf(site,500,
+      "<html>\
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+      <body>\
+      <h1>Reset ESP</h1>\
+      <form action=\"/resetESP\">\
+      <input type=\"submit\" value=\"RESET\">\
+      </form>\
+      </body>\
+      </html>"
+      );
+  Serial.println("Finish running updateAccessPoint");
+  _webServer.send(200,"text/html",site);
+}
+void SmartWifi::resetESPHandler(void){
+  WiFi.disconnect();
+  ESP.restart();//After the first flash this command wan't work
+  //Need to reset the esp to make the command work
 }
 
 uint8_t lastConnection;
@@ -110,54 +251,6 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
   }
 }
 
-void newAccessPoint(){
-  char site[300];
-  snprintf(site,300,
-      "<html>\
-      <body>\
-      <h1>Update Access Point Settings</h1>\
-      <form action=\"/updatedAccessPoint\">\
-      Access Point:<input type=\"text\" name=\"Access Point\">\
-      <br>\
-      Password:<input type=\"text\" name=\"Password\">\
-      <br>\
-      <input type=\"submit\" value=\"Update\">\
-      </form>\
-      </body>\
-      </html>"
-      );
-  server.send(200,"text/html",site);
-}
-
-void updateAcessPoint(void){
-  Serial.println(server.argName(0) + ": " + server.arg(0));
-  Serial.println(server.argName(1) + ": " + server.arg(1));
-
-  server.arg(0).toCharArray(ssid,32);
-  server.arg(1).toCharArray(password,16);
-  /*
-     --- Here Come the code for saving the data into the EEPROM
-   */
-  saveCredentials();
-  char site[500];
-  snprintf(site,500,
-      "<html>\
-      <body>\
-      <h1>Reset ESP</h1>\
-      <form action=\"/resetESP\">\
-      <input type=\"submit\" value=\"RESET\">\
-      </form>\
-      </body>\
-      </html>"
-      );
-  Serial.println("Finish running updateAccessPoint");
-  server.send(200,"text/html",site);
-}
-void resetESP(void){
-  WiFi.disconnect();
-  ESP.restart();//After the first flash this command wan't work
-  //Need to reset the esp to make the command work
-}
 
 int readI2C(int addressBus,byte controlReg){
   Wire.beginTransmission(addressBus); // wake up PCF8591
@@ -169,7 +262,11 @@ int readI2C(int addressBus,byte controlReg){
 }
 
 void setup() {
+  Serial.begin(115200);
+  SmartWifi wifiConnection(0, 4096, true, webServer);
+  wifiConnection.connectToWifi();
   //Configure The Serial Settings
+  /*
   Serial.begin(115200);
   Serial.setTimeout(0.05);
   //clearCredentials();
@@ -209,9 +306,11 @@ void setup() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
   }
+  */
 }
 
 void loop() {
+  /*
   webSocket.loop();
   //The full message form is (number Of Analog,Analog Value 0,Analog Value 1,...)
   if (isConnected){
@@ -225,4 +324,5 @@ void loop() {
     webSocket.sendTXT(lastConnection, textToSend);
     delay(300);
   }
+  */
 }
